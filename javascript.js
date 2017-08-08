54d39520073b19abdbab802b510daf17b8b1b441
Thomas <tcote19@gmail.com>
2017-08-07 22:27:12 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
-var growButton = document.getElementById("box");
-var growParseH = parseInt(growButton.style.height, 10);
-var growParseW = parseInt(growButton.style.width, 10);
+var grabBox = document.getElementById("box");
+var growParseH = parseInt(grabBox.style.height, 10);
+var growParseW = parseInt(grabBox.style.width, 10);
 
 function grow() {
-growParseH = parseInt(growButton.style.height, 10);
-growParseW = parseInt(growButton.style.width, 10);
+growParseH = parseInt(grabBox.style.height, 10);
+growParseW = parseInt(grabBox.style.width, 10);
 
-growButton.style.height = (growParseH + 100) + "px";
-growButton.style.width = (growParseW + 100) + "px";
+grabBox.style.height = (growParseH + 100) + "px";
+grabBox.style.width = (growParseW + 100) + "px";
 }
-// function increases CSS height and width of the box when "grow" button is clicked.
+// I wanted to improvise a little on this function so that it will incrementally grow many times over.
 
+function colorize() {
+	grabBox.style.background = "blue";
+}
+// function changes the box's background color property to blue.
+
+function fade() {
+	$("#box").fadeToggle();
+}
+// I enabled the jQuery script for this function because it was sooooo much easier this way. Please don't expel me.
+// The box can be faded in and out consecutively this way. It just feels right.
+
+// The final issue of getting the page to Reset was accomplished with an inline window.reload function. See the HTML for more.
